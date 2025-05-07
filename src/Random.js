81b418624bab2becc73978dc1b0c617726c0e5a8
OSJ <oussama.0rc61b@gmail.com>
2025-05-07 18:53:40 +0200

    commit 1 : First commit

---
+++ src/Random.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+
+
+const Random = () => {
+
+    const [num, setNum] = useState(0);
+    function randomNumberInRange(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+      }
+    
+    const handleClick = () => {
+        setNum(randomNumberInRange(0, 59));
+      };
+
+    return (  
+    
+    <div>
+      <h2>nombre aléatoire : {num}</h2>
+      <button onClick={handleClick}>Générer un nombre aléatoire</button>
+    </div>
+    
+    );
+}
+ 
+export default Random;
